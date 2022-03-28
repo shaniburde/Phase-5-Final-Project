9ce984330e9cbd803e69ac7807af776fc70c3de5
Shani burde <shaniburde@Shanis-Air.home>
2022-03-27 21:36:31 -0400

    More css

--- client/src/About.js
+++ client/src/About.js
@@ -9,6 +9,8 @@ export default function About() {
         <br/>
         MissFit is a personal closet management app that ensures that you're the best dressed in the room.
         <br/>
+        <br/>
+        <h5>Here's this week's weather forecast, choose an outfit accordingly!</h5>
         <WeekContainer />
     </div>
   )
--- client/src/DayCard.js
+++ client/src/DayCard.js
@@ -10,7 +10,7 @@ const DayCard = ({ reading }) => {
 
   return (
       <div className="col">
-        <div className="card" style={{borderRadius: "35px", padding: "5%"}}>
+        <div className="card" style={{borderRadius: "35px", padding: "6%", backgroundColor: "#E0F5F2", border: "none"}}>
             <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
             <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
             <i className={imgURL}></i>
